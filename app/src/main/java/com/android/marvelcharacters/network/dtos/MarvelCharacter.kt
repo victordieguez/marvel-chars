1bd9a5da6d899cf0395f9f4603f746e3ee19d6e3
Victor Dieguez <victor.dieguezrubio@gmail.com>
2020-11-21 13:37:11 +0100

    Date can come wrong sometimes

--- app/src/main/java/com/android/marvelcharacters/network/dtos/MarvelCharacter.kt
+++ app/src/main/java/com/android/marvelcharacters/network/dtos/MarvelCharacter.kt
@@ -1,8 +1,6 @@
 package com.android.marvelcharacters.network.dtos
 
-import java.util.Date
-
-class MarvelCharacter(val id: Int, val name: String, val description: String, val modified: Date, private val thumbnail: MarvelCharacterImage) {
+class MarvelCharacter(val id: Int, val name: String, val description: String, val modified: String, private val thumbnail: MarvelCharacterImage) {
 
     fun getImage(): String {
         return thumbnail.path + "." + thumbnail.extension
